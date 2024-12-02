caf3eb0bf5ab63e8a4e81ec08bb84325ed18ac9a
dom <domwilson95@hotmail.co.uk>
2024-12-02 10:27:33 +0000

    day2 faster

--- src/day2.rs
+++ src/day2.rs
@@ -30,6 +30,21 @@ fn solver_part1(input: &Input) -> usize {
         .count()
 }
 
+#[aoc(day2, part1, FAST)]
+fn solver_part1_fast(input: &Input) -> usize {
+    input
+        .iter()
+        .filter(|vec| {
+            //
+            let sign = (vec[1] - vec[0]).signum();
+            vec.iter().tuple_windows().all(|(a, b)| {
+                let abs = (b - a).abs();
+                (1..=3).contains(&abs) && (b - a).signum() == sign
+            })
+        })
+        .count()
+}
+
 #[aoc(day2, part2)]
 fn solver_part2(input: &Input) -> usize {
     input
@@ -46,6 +61,24 @@ fn solver_part2(input: &Input) -> usize {
                 let sign = diff.iter().map(|v| v.signum()).collect_vec();
                 let mag = diff.iter().map(|v| v.abs()).collect_vec();
                 mag.iter().all(|v| *v <= 3 && *v >= 1) && sign.iter().all_equal()
+            })
+        })
+        .count()
+}
+
+#[aoc(day2, part2, FAST)]
+fn solver_part2_fast(input: &Input) -> usize {
+    input
+        .iter()
+        .filter(|vec| {
+            //
+
+            vec.iter().combinations(vec.len() - 1).any(|vec| {
+                let sign = (vec[1] - vec[0]).signum();
+                vec.into_iter().tuple_windows().all(|(a, b)| {
+                    let abs = (*b - *a).abs();
+                    (1..=3).contains(&abs) && (*b - *a).signum() == sign
+                })
             })
         })
         .count()

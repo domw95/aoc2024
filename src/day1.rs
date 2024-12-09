593cfdd96e5231c60e16ff0c7118ff1e8bf5d163
dom <domwilson95@hotmail.co.uk>
2024-12-09 16:55:32 +0000

    day1 performance

--- src/day1.rs
+++ src/day1.rs
@@ -1,12 +1,17 @@
 use std::collections::HashMap;
+use std::thread;
 
 use aoc_runner_derive::aoc;
 use aoc_runner_derive::aoc_generator;
 
-type Input = (Vec<i64>, Vec<i64>);
+type Input = String;
 
 #[aoc_generator(day1)]
 fn input_generator(input: &str) -> Input {
+    input.to_string()
+}
+
+fn parse(input: &str) -> (Vec<i64>, Vec<i64>) {
     input
         .lines()
         .map(|l| {
@@ -17,9 +22,31 @@ fn input_generator(input: &str) -> Input {
         .unzip()
 }
 
+fn parse_i32(input: &str) -> (Vec<i32>, Vec<i32>) {
+    input
+        .lines()
+        .map(|l| {
+            //
+            let (a, b) = l.split_once("   ").unwrap();
+            (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap())
+        })
+        .unzip()
+}
+
+fn fast_parse_i32(input: &str) -> (Vec<i32>, Vec<i32>) {
+    input
+        .lines()
+        .map(|l| {
+            //
+            let (a, b) = l.split_once("   ").unwrap();
+            (a.parse::<i32>().unwrap_or(0), b.parse::<i32>().unwrap_or(0))
+        })
+        .unzip()
+}
+
 #[aoc(day1, part1)]
 fn solver_part1(input: &Input) -> i64 {
-    let mut input = input.clone();
+    let mut input = parse(input);
     input.0.sort();
     input.1.sort();
     input
@@ -33,8 +60,92 @@ fn solver_part1(input: &Input) -> i64 {
         .sum()
 }
 
-#[aoc(day1, part2, FILTER)]
-fn solver_part2_filter(input: &Input) -> usize {
+#[aoc(day1, part1, UNSTABLE)]
+fn solver_part1_unstable(input: &Input) -> i64 {
+    let mut input = parse(input);
+    input.0.sort_unstable();
+    input.1.sort_unstable();
+    input
+        .0
+        .iter()
+        .zip(input.1)
+        .map(|(a, b)| {
+            //
+            (a - b).abs()
+        })
+        .sum()
+}
+
+#[aoc(day1, part1, UNSTABLE_I32)]
+fn solver_part1_unstable_i32(input: &Input) -> i32 {
+    let mut input = parse_i32(input);
+    input.0.sort_unstable();
+    input.1.sort_unstable();
+    input
+        .0
+        .iter()
+        .zip(input.1)
+        .map(|(a, b)| {
+            //
+            (a - b).abs()
+        })
+        .sum()
+}
+
+#[aoc(day1, part1, UNSTABLE_I32_PARALLEL)]
+fn solver_part1_unstable_i32_parallel(input: &Input) -> i32 {
+    let (mut a, mut b) = parse_i32(input);
+    let handle = thread::spawn(|| {
+        a.sort_unstable();
+        a
+    });
+    b.sort_unstable();
+    let a = handle.join().unwrap();
+    a.iter()
+        .zip(b)
+        .map(|(a, b)| {
+            //
+            (a - b).abs()
+        })
+        .sum()
+}
+
+#[aoc(day1, part1, UNSTABLE_I32_FAST)]
+fn solver_part1_unstable_i32_fast(input: &Input) -> i32 {
+    let mut input = fast_parse_i32(input);
+    input.0.sort_unstable();
+    input.1.sort_unstable();
+    input
+        .0
+        .iter()
+        .zip(input.1)
+        .map(|(a, b)| {
+            //
+            (a - b).abs()
+        })
+        .sum()
+}
+
+fn parse_map(input: &str) -> (Vec<i32>, HashMap<i32, i32>) {
+    let mut vec = Vec::new();
+    let mut map = HashMap::new();
+    for l in input.lines() {
+        let (a, b) = l.split_once("   ").unwrap();
+        let (a, b) = (a.parse::<i32>().unwrap_or(0), b.parse::<i32>().unwrap_or(0));
+        match map.get_mut(&b) {
+            Some(v) => *v += 1,
+            None => {
+                map.insert(b, 1);
+            }
+        };
+        vec.push(a);
+    }
+    (vec, map)
+}
+
+#[aoc(day1, part2)]
+fn solver_part2(input: &Input) -> usize {
+    let input = parse(input);
     input
         .0
         .iter()
@@ -44,6 +155,7 @@ fn solver_part2_filter(input: &Input) -> usize {
 
 #[aoc(day1, part2, HASH)]
 fn solver_part2_hash(input: &Input) -> i64 {
+    let input = parse(input);
     let mut hash = HashMap::new();
     for v in &input.1 {
         match hash.get(v) {
@@ -65,6 +177,22 @@ fn solver_part2_hash(input: &Input) -> i64 {
         .sum()
 }
 
+#[aoc(day1, part2, HASH_INLINE)]
+fn solver_part2_hash_inline(input: &Input) -> i32 {
+    let input = parse_map(input);
+
+    input
+        .0
+        .iter()
+        .map(|a| {
+            //
+            match input.1.get(a) {
+                Some(x) => a * x,
+                None => 0,
+            }
+        })
+        .sum()
+}
 #[cfg(test)]
 mod tests {
     use super::input_generator;

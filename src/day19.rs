c1099f42ea5b486a6774a30d5f594c5afcb8d491
dom <domwilson95@hotmail.co.uk>
2024-12-19 15:16:16 +0000

    day19 performance

--- src/day19.rs
+++ src/day19.rs
@@ -1,9 +1,17 @@
 #![allow(clippy::comparison_chain)]
 use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 use aoc_runner_derive::aoc;
 use aoc_runner_derive::aoc_generator;
+use dashmap::DashMap;
+use fxhash::FxHashMap;
 use itertools::Itertools;
+use rayon::iter::IntoParallelIterator;
+use rayon::iter::ParallelBridge;
+use rayon::iter::ParallelIterator;
+use rayon::slice::ParallelSlice;
 
 type Input = String;
 
@@ -67,6 +75,64 @@ fn possible_pattern_count<'a>(
     }
 }
 
+fn possible_pattern_count_fx<'a>(
+    pattern: &'a [u8],
+    towels: &[&[u8]],
+    cache: &mut FxHashMap<&'a [u8], usize>,
+) -> usize {
+    // println!("Checking {}", String::from_utf8(pattern.to_vec()).unwrap());
+    if let Some(count) = cache.get(pattern) {
+        *count
+    } else {
+        let count = towels
+            .iter()
+            .map(|&towel| {
+                // println!("? {}", String::from_utf8(towel.to_vec()).unwrap());
+                if pattern == towel {
+                    1
+                } else if towel.len() < pattern.len() {
+                    if towel.iter().zip(pattern.iter()).all(|(a, b)| a == b) {
+                        // println!("Match {}", String::from_utf8(towel.to_vec()).unwrap());
+                        possible_pattern_count_fx(&pattern[towel.len()..], towels, cache)
+                    } else {
+                        0
+                    }
+                } else {
+                    0
+                }
+            })
+            .sum();
+        cache.insert(pattern, count);
+        count
+    }
+}
+
+fn possible_pattern_count_dash<'a>(
+    pattern: &'a [u8],
+    towels: &[&[u8]],
+    cache: &DashMap<&'a [u8], usize>,
+) -> usize {
+    // println!("Checking {}", String::from_utf8(pattern.to_vec()).unwrap());
+    if let Some(count) = cache.get(pattern) {
+        return *count;
+    }
+
+    let mut count = 0;
+    for &towel in towels {
+        // println!("? {}", String::from_utf8(towel.to_vec()).unwrap());
+        if pattern == towel {
+            count += 1
+        } else if towel.len() < pattern.len()
+            && towel.iter().zip(pattern.iter()).all(|(a, b)| a == b)
+        {
+            // println!("Match {}", String::from_utf8(towel.to_vec()).unwrap());
+            count += possible_pattern_count_dash(&pattern[towel.len()..], towels, cache);
+        }
+    }
+    cache.insert(pattern, count);
+    count
+}
+
 #[aoc(day19, part1)]
 fn solver_part1(input: &Input) -> usize {
     let mut lines = input.lines();
@@ -78,6 +144,23 @@ fn solver_part1(input: &Input) -> usize {
         .collect_vec();
     lines.next();
     lines
+        .map(str::as_bytes)
+        .filter(|&pattern| possible_pattern(pattern, &towels))
+        .count()
+}
+
+#[aoc(day19, part1, PARALLEL)]
+fn solver_part1_parallel(input: &Input) -> usize {
+    let mut lines = input.lines();
+    let towels = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|str| str.trim().as_bytes())
+        .collect_vec();
+    lines.next();
+    lines
+        .par_bridge()
         .map(str::as_bytes)
         .filter(|&pattern| possible_pattern(pattern, &towels))
         .count()
@@ -100,6 +183,141 @@ fn solver_part2(input: &Input) -> usize {
         .sum()
 }
 
+#[aoc(day19, part2, PARALLEL)]
+fn solver_part2_parallel(input: &Input) -> usize {
+    let mut lines = input.lines();
+    let cache = HashMap::new();
+    let towels = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|str| str.trim().as_bytes())
+        .collect_vec();
+    lines.next();
+    lines
+        .par_bridge()
+        .map(str::as_bytes)
+        .map(|pattern| possible_pattern_count(pattern, &towels, &mut cache.clone()))
+        .sum()
+}
+
+#[aoc(day19, part2, PARALLEL_DASH)]
+fn solver_part2_parallel_dash(input: &Input) -> usize {
+    let mut lines = input.lines();
+    let cache = DashMap::new();
+    let towels = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|str| str.trim().as_bytes())
+        .collect_vec();
+    lines.next();
+    lines
+        .par_bridge()
+        .map(str::as_bytes)
+        .map(|pattern| possible_pattern_count_dash(pattern, &towels, &cache))
+        .sum()
+}
+
+#[aoc(day19, part2, PARALLEL_WITH)]
+fn solver_part2_parallel_with(input: &Input) -> usize {
+    let mut lines = input.lines();
+    let cache = HashMap::new();
+    let towels = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|str| str.trim().as_bytes())
+        .collect_vec();
+    lines.next();
+    lines
+        .par_bridge()
+        .map(str::as_bytes)
+        .map_with(cache, |cache, pattern| {
+            possible_pattern_count(pattern, &towels, cache)
+        })
+        .sum()
+}
+
+#[aoc(day19, part2, PARALLEL_CHUNKS)]
+fn solver_part2_parallel_chunks(input: &Input) -> usize {
+    let mut lines = input.lines();
+    let cache = HashMap::new();
+    let towels = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|str| str.trim().as_bytes())
+        .collect_vec();
+    lines.next();
+    let lines = lines.collect_vec();
+    lines
+        .par_chunks(5)
+        .map(|chunk| {
+            chunk
+                .iter()
+                .map(|str| possible_pattern_count(str.as_bytes(), &towels, &mut cache.clone()))
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+#[aoc(day19, part2, PARALLEL_WITH_FX)]
+fn solver_part2_parallel_with_fx(input: &Input) -> usize {
+    let mut lines = input.lines();
+    let cache = FxHashMap::default();
+    let towels = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|str| str.trim().as_bytes())
+        .collect_vec();
+    lines.next();
+    lines
+        .par_bridge()
+        .map(str::as_bytes)
+        .map_with(cache, |cache, pattern| {
+            possible_pattern_count_fx(pattern, &towels, cache)
+        })
+        .sum()
+}
+
+#[aoc(day19, part2, PARALLEL_MANUAL)]
+fn solver_part2_parallel_manual(input: &Input) -> usize {
+    let mut lines = input.lines();
+
+    let towels = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|str| str.trim().as_bytes())
+        .collect_vec();
+    lines.next();
+    let lines = Arc::new(Mutex::new(lines));
+    let global_cache = Arc::new(Mutex::new(HashMap::new()));
+    (0..12)
+        .into_par_iter()
+        .map(|_| {
+            let mut count = 0;
+            let lines = lines.clone();
+            let mut cache = HashMap::new();
+            loop {
+                let mut guard = lines.lock().unwrap();
+                if let Some(line) = guard.next() {
+                    drop(guard);
+                    // let mut cache = global_cache.lock().unwrap().clone();
+                    count += possible_pattern_count(line.as_bytes(), &towels, &mut cache);
+                    let mut global = global_cache.lock().unwrap();
+                    global.extend(cache);
+                    cache = global.clone();
+                } else {
+                    break;
+                }
+            }
+            count
+        })
+        .sum()
+}
 #[cfg(test)]
 mod tests {
     use super::input_generator;
